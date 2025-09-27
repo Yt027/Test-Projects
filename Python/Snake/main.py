391dc39f106bb605a0863c71f8f621445829c9b9
Yt027 <yayatangara027@gmail.com>
2025-09-27 15:17:33 +0000

    Got a functional pygame window. Houra!

--- Python/Snake/main.py
+++ Python/Snake/main.py
@@ -1,3 +1,19 @@
 from random import randint
-import pygame
+import pygame, sys
 pygame.init()
+
+
+screen = pygame.display.set_mode((600, 500))
+pygame.display.set_caption("Snake.py")
+
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+
+if __name__ == "__main__":
+    main()
