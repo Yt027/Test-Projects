8e3ff83c2740ff3bd5e294d9a91b8fa335e0bfae
Yt027 <yayatangara027@gmail.com>
2025-10-09 00:00:04 +0000

    Hero finished

--- React/sugu/src/Shop.jsx
+++ React/sugu/src/Shop.jsx
@@ -11,11 +11,18 @@ function Shop() {
         <>
             <Header />
 
-            <Sidebar />
-
-            <div className="hero flex flex-col p-3">
-                <SearchBox input={searchInput} setInput={setSearchInput}  />
+            <div 
+                className="hero z-1 w-full h-70 
+                    bg-[url(/hero.jpg)] 
+                    bg-cover flex items-end 
+                    justify-center p-1 pb-3 mb-10"
+            >
+                <div className="hero flex flex-col p-3">
+                    <SearchBox className="z-10" input={searchInput} setInput={setSearchInput}  />
+                </div>
             </div>
+
+            <Sidebar />
 
             <div
                 className="products flex flex-wrap gap-4 justify-center p-3"
